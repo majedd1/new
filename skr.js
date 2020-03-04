8e7f9dc2045418a60097dd959505fedc89abdb8d
majedd1 <57370951+majedd1@users.noreply.github.com>
2020-03-04 18:34:41 +0300

    Create skr.js

---
+++ skr.js
@@ -0,0 +1,27 @@
+$(`<img src="jawall.gif" class="fr" style="width:100%;margin-right: 2px;">
+
+<div style="border-radius: 2px 9px 9px 9px;width:100%;margin:2px;border: 2px solid #32639C;"  id="asim33" class="btn btn-primary btn-sm" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample"> ادارة الموقع</div><div class="collapse" id="collapseExample"><center><div class="well">
+<b>
+<font color="#78939C"> اداره  الموقع </font> </BR>
+</BR>  Deem , </BR>
+ </BR> صــــاحب الموقع </BR>
+<font color="#BF1D6E">яєємα</font></BR>
+
+`).insertBefore('.nav-tabs');
+
+$(`<div class="uzr fl corner borderg mm" style="border-radius:5px;margin-bottom:-2px;width:99.5%;padding:0px; background-color:#F7E7E7;">
+<img style="width: 36px; height: 38px; margin-left: 1px; margin-top: 1px; background-image: url;" class="fl fitimg hand u-pic ">
+<div class="uzr fl" style="padding:0px;width:80%">
+<div style="padding:0px;width:100%;" class="fl">
+<img class="fl u-ico" alt="" src="">
+<span style="padding: 1px 8px; margin-top: 2px; display: block; max-width: 82%; border-radius: 3px; color: rgb(199, 103, 48);" class="corner nosel u-topic dots fl hand">‎ .. ' яєємα.. صاحب الموقع ، ،</span>
+</div>
+<br>
+<div style="padding: 0px; width: 100%; color: rgb(60, 0, 255);" class=" u-msg break fl">
+<div style="padding: 0px;width: 100%;color: rgb(60, 0, 255);text-align: left;display: block;margin-top: -17px;" class=" u-msg break fl"> مرحباً بك عزيزي الزآئر 👋 <h1 style="display: inline-block;color: red;">`+ getuser(myid).topic+ `</h1> نقدّر تواجدك معنا، ونتمنى لك يوماً سعيداً إن شاء الله </div>
+</div>
+</div></div>
+
+`).appendTo('div#d2')
+clearInterval(myVar);
+}else{console.log(usmsgw)} }, 2000);})

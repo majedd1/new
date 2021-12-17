9e449b2a56be5e8b0bbbc9367b75056bd369e4c6
majedd1 <57370951+majedd1@users.noreply.github.com>
2021-12-17 16:15:43 +0300

    Create ohoh.js

---
+++ ohoh.js
@@ -0,0 +1,7 @@
+if(location.protocol!='https:')
+{location.protocol='https:';} 
+$(`<img src="/sico/1623138096611.gif" class="fr" style="width:100%;margin-top: -5px;" ></a> `).insertBefore('.nav-tabs');
+$('.break.light').css({"background-image":"url(/sico/1631135298921.jpg"}); 
+$(` </style><link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera"> <link rel="stylesheet" href="https://cdn.statically.io/gh/majedd1/m/5cf02764/ssa.css"> </style>`).insertBefore('body');
+$(".fa-bolt").text(" ");
+$('.fa-gear').text(' ')

41ce1eab72a396f09467203245012ad2bd3d4c75
majedd1 <57370951+majedd1@users.noreply.github.com>
2022-07-14 23:45:44 +0300

    Create newaboo2.js

---
+++ newaboo2.js
@@ -0,0 +1,16 @@
+if(location.protocol!='https:')
+{location.protocol='https:';} 
+$('#users').css('background', '#fff !important');
+localStorage.removeItem('fp1'); bitrate = 128 
+
+$(`<img src="https://jzan1.com/sico/1654795569469.gif" class="fr" style="width:100%;margin-top: -5px;" ></a> `).insertBefore('.nav-tabs');
+$(` </style><link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera"> <link rel="stylesheet" href="https://rawcdn.githack.com/majedd1/new/781e2016586df995c846860834593ef6852fd764/newaboo2.css"> </style>`).insertBefore('body');
+$('.fa-gear').text(' ')
+$(`<style>
+.fl.nosel.label.pnhead{border:1px dashed#e75c8b!important;background:#a5a1a3!important;border-radius:20px;font-family:'jazeera',FontAwesome;font-size:14px!important;box-shadow:inset -1px -1px 1px rgb(220 104 143),0 0 8px rgb(255 255 255)}.broadcasters>div>.prod{background-image:url(sico/1654797991189.gif);border-radius:15px 5px!important} \ .hand.nosel.fl.uzr.border{width:99%!important;margin-top:5px!important;border-radius:3px!important;border:1px solid #1391a740} img.fitimg.fl.borderg.spic.corner.hand { background-size: 100% 100%; width:80px!important; padding:20px!important; margin-left: 18%!important; border: 2px solid #e9598a7d!important; border-radius: 100px!important;}  .checkbox {color: #000000;} .fl.mini.u-msg{width:190px!important}.fl.mini.u-msg{display:inline!important;white-space:break-spaces!important;padding:7px;text-align:center}
+tbox{padding:0px;min-height:28px!important;border-right:5px solid;border-left:5px solid;border-color:#d2d2d2;border-left-style:groove;border-right-style:ridge;border-radius:3px!important} .fr.co{box-shadow:inset 1px 1px 1px 1px rgba(152,106,136,.21176470588235294),0 0 5px #986a88}
+.broadcasters {   background: #051626 !important;border-bottom: 2px solid #ca918f !important;background-image: url(sico/1654798004447.gif)!important;}
+</style>`).insertBefore('body');
+$('.pro').css('background-image','url(sico/1654796460638.gif)');
+$('.sitel > img').remove()
+$('ul.nav.nav-tabs').css('padding','3px 0px');

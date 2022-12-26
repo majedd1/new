d28131e0eccb2d05c2226dec2f086100cc8d8ec8
majedd1 <57370951+majedd1@users.noreply.github.com>
2022-12-26 06:42:56 +0300

    Create 3skr.js

---
+++ 3skr.js
@@ -0,0 +1,9 @@
+$('.sitel').removeClass('fl');
+$('.sitel').addClass('fr');
+$('.sitel').css('text-align','left');
+$('.sitel').removeClass('fl');
+$('.sitel').addClass('fr');
+$('.sitel').css('text-align','left');
+$(`<img src="https://abo-arish.net/sico/1669820925662.gif" class="fr" style="width:99.5%;margin-top: 0px;" ></a> `).insertBefore('.nav-tabs');
+$('.fa-gear').text('الضبط') 
+$(` </style><link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera"> <link rel="stylesheet" href="https://rawcdn.githack.com/majedd1/new/e86bde86fa9f05edf370845f1d1c15474cb99c51/111N.css"> </style>`).insertBefore('body');

7623270a3e31e9684753ecb7298f6f1dba0cb215
majedd1 <57370951+majedd1@users.noreply.github.com>
2022-12-26 07:06:51 +0300

    Create 3net.js

---
+++ 3net.js
@@ -0,0 +1,9 @@
+$('.sitel').removeClass('fl');
+$('.sitel').addClass('fr');
+$('.sitel').css('text-align','left');
+$('.sitel').removeClass('fl');
+$('.sitel').addClass('fr');
+$('.sitel').css('text-align','left');
+$(`<img src="https://abo-arish.net/sico/1669820901787.gif" class="fr" style="width:99.5%;margin-top: 0px;" ></a> `).insertBefore('.nav-tabs');
+$('.fa-gear').text('الضبط') 
+$(` </style><link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera"> <link rel="stylesheet" href="https://rawcdn.githack.com/majedd1/new/46b825e1f9a1dd1b5b64130b6ccb83831251e51b/111N.css"> </style>`).insertBefore('body');

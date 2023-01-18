aab1eec9701d9f046ec2c5da21da6fe7c3fb3762
majedd1 <57370951+majedd1@users.noreply.github.com>
2023-01-18 05:46:59 +0300

    Update 3net.js

--- 3net.js
+++ 3net.js
@@ -1,9 +1,1 @@
-$('.sitel').removeClass('fl');
-$('.sitel').addClass('fr');
-$('.sitel').css('text-align','left');
-$('.sitel').removeClass('fl');
-$('.sitel').addClass('fr');
-$('.sitel').css('text-align','left');
-$(`<img src="https://abo-arish.net/sico/1669820901787.gif" class="fr" style="width:99.5%;margin-top: 0px;" ></a> `).insertBefore('.nav-tabs');
-$('.fa-gear').text('الضبط') 
-$(` </style><link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera"> <link rel="stylesheet" href="https://rawcdn.githack.com/majedd1/new/46b825e1f9a1dd1b5b64130b6ccb83831251e51b/111N.css"> </style>`).insertBefore('body');
+$('.sitel').removeClass('fl');$('.sitel').addClass('fr');$('.sitel').css('text-align','left');$('.sitel').removeClass('fl');$('.sitel').addClass('fr');$('.sitel').css('text-align','left');$(`<img src="https://abo-arish.net/sico/1669820901787.gif" class="fr" style="width:99.5%;margin-top: 0px;" ></a> `).insertBefore('.nav-tabs');$('.fa-gear').text('الضبط') $(` </style><link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera"> <link rel="stylesheet" href="https://rawcdn.githack.com/majedd1/new/4f1ff4ab6b150488d60461c388728fc7c664938d/1N.css"> </style>`).insertBefore('body');

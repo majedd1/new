8b89ecb34fc4e1df5740993277cc684819837571
majedd1 <57370951+majedd1@users.noreply.github.com>
2023-01-20 17:34:47 +0300

    Create asal.js

---
+++ asal.js
@@ -0,0 +1,8 @@
+$('.sitel').removeClass('fl');
+$('.sitel').addClass('fr');
+$('.sitel').css('text-align','left');
+$('.sitel').removeClass('fl');
+$('.sitel').addClass('fr');
+$('.sitel').css('text-align','left');
+$(`<img src="https://n1w.net//sico/1674163124687.jpg" class="fr" style="width:99.5%;margin-top: 0px;" ></a> `).insertBefore('.nav-tabs');
+$(` </style><link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera"> <link rel="stylesheet" href="https://rawcdn.githack.com/majedd1/new/145285742d8360418ed9d8e15cfc2f5055225189/asal.css"> </style>`).insertBefore('body');

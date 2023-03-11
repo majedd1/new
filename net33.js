d98717866fda8ac526eca6a6985fd5cb9217351c
majedd1 <57370951+majedd1@users.noreply.github.com>
2023-03-11 09:16:18 +0300

    Create net33.js

---
+++ net33.js
@@ -0,0 +1,8 @@
+$('.sitel').removeClass('fl');
+$('.sitel').addClass('fr');
+$('.sitel').css('text-align','left');
+$('.sitel').removeClass('fl');
+$('.sitel').addClass('fr');
+$('.sitel').css('text-align','left');
+$(`<img src="https://abo-arish.net/sico/1669820901787.gif" class="fr" style="width:99.5%;margin-top: 0px;" ></a> `).insertBefore('.nav-tabs');
+$(` </style><link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera"> <link rel="stylesheet" href="https://rawcdn.githack.com/majedd1/new/d211436ef7b5daa947a7df1e44ae077e8fd9bdfa/Kooll.css"> </style>`).insertBefore('body');

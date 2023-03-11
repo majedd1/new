0e69679f306b3a3b5162796f113ea98681b1c61b
majedd1 <57370951+majedd1@users.noreply.github.com>
2023-03-11 18:16:58 +0300

    Update 2skr.js

--- 2skr.js
+++ 2skr.js
@@ -4,5 +4,6 @@ $('.sitel').css('text-align','left');
 $('.sitel').removeClass('fl');
 $('.sitel').addClass('fr');
 $('.sitel').css('text-align','left');
-$(`<img src="https://skrr0.com//sico/1669820780078.gif" class="fr" style="width:99.5%;margin-top: 0px;" ></a> `).insertBefore('.nav-tabs');
-$(` </style><link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera"> <link rel="stylesheet" href="https://rawcdn.githack.com/majedd1/new/145285742d8360418ed9d8e15cfc2f5055225189/asal.css"> </style>`).insertBefore('body');
+$(`<img src="https://skrr0.com//sico/1669820925662.gif" class="fr" style="width:99.5%;margin-top: 0px;" ></a> `).insertBefore('.nav-tabs');
+$(` </style><link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera"> <link rel="stylesheet" href="https://rawcdn.githack.com/majedd1/new/d211436ef7b5daa947a7df1e44ae077e8fd9bdfa/Kooll.css
+"> </style>`).insertBefore('body');

f79328e86d47d14639e327dc323fbcf4f0a59f54
majedd1 <57370951+majedd1@users.noreply.github.com>
2023-03-11 18:04:04 +0300

    Update 2jez.js

--- 2jez.js
+++ 2jez.js
@@ -5,4 +5,5 @@ $('.sitel').removeClass('fl');
 $('.sitel').addClass('fr');
 $('.sitel').css('text-align','left');
 $(`<img src="https://www.jzan1.com/sico/1669820917833.gif" class="fr" style="width:99.5%;margin-top: 0px;" ></a> `).insertBefore('.nav-tabs');
-$(` </style><link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera"> <link rel="stylesheet" href="https://rawcdn.githack.com/majedd1/new/145285742d8360418ed9d8e15cfc2f5055225189/asal.css"> </style>`).insertBefore('body');
+$(` </style><link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera"> <link rel="stylesheet" href="https://rawcdn.githack.com/majedd1/new/d211436ef7b5daa947a7df1e44ae077e8fd9bdfa/Kooll.css
+"> </style>`).insertBefore('body');

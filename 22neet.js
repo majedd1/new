55a9aa3347b5c2240883711168f3ba0f9a87114d
majedd1 <57370951+majedd1@users.noreply.github.com>
2023-04-19 06:30:19 +0300

    Update 22neet.js

--- 22neet.js
+++ 22neet.js
@@ -5,4 +5,4 @@ $('.sitel').removeClass('fl');
 $('.sitel').addClass('fr');
 $('.sitel').css('text-align','left');
 $(`<img src="https://abo-arish.net/sico/1669820901787.gif" class="fr" style="width:99.5%;margin-top: 0px;" ></a> `).insertBefore('.nav-tabs');
-$(` </style><link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera"> <link rel="stylesheet" href="https://rawcdn.githack.com/majedd1/new/145285742d8360418ed9d8e15cfc2f5055225189/asal.css"> </style>`).insertBefore('body');
+$(` </style><link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera"> <link rel="stylesheet" href="https://cdn.statically.io/gh/majedd1/new/85378587/Kooll.css"> </style>`).insertBefore('body');

abe845c65054afedc56df22054f5eeaba4048184
majedd1 <57370951+majedd1@users.noreply.github.com>
2023-04-19 07:04:18 +0300

    Update 2com.js

--- 2com.js
+++ 2com.js
@@ -5,5 +5,5 @@ $('.sitel').removeClass('fl');
 $('.sitel').addClass('fr');
 $('.sitel').css('text-align','left');
 $(`<img src="https://abo-arish.com/sico/1669820812667.gif" class="fr" style="width:99.5%;margin-top: 0px;" ></a> `).insertBefore('.nav-tabs');
-$(` </style><link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera"> <link rel="stylesheet" href="https://rawcdn.githack.com/majedd1/new/d211436ef7b5daa947a7df1e44ae077e8fd9bdfa/Kooll.css
+$(` </style><link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera"> <link rel="stylesheet" href="https://cdn.statically.io/gh/majedd1/new/85378587/Kooll.css
 "> </style>`).insertBefore('body');
